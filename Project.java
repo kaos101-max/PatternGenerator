8c9dd84a79e216fdc650526676021736dda7ddf9
kaos101-max <watanabedaisuke616@gmail.com>
2025-10-30 18:08:28 +0000

    CobaltDragoon

--- Project.java
+++ Project.java
@@ -1,1 +1,13 @@
-
+//Name: Daisuke Watanabe
+//Date: 10/30/25
+//Description:
+public class Pattern{
+    //precondtion
+    //postcondition
+    public static void stars(int rwos){
+        //code goes here
+    }
+    public static void main(String[] args){
+        stars(5);
+    }
+}
