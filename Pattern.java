c51a1e5c6d3a92e067378ffa995e8bdb1bb4eab3
kaos101-max <watanabedaisuke616@gmail.com>
2025-11-11 19:28:50 +0000

    Iconic

--- Pattern.java
+++ Pattern.java
@@ -6,6 +6,7 @@ public class Pattern{
     //postcondition
     public static void main(String[] args){
         stars(7);
+        triangle(9);
     }
     public static void stars(int rows){
         //code goes here
@@ -17,19 +18,18 @@ public class Pattern{
         stars++;
         }
     }
-    // public static void main(String[] args){
-    //     num (9);
-    // public static void triangle(int rows){
-    //     int num = 1;
-    //     int output1 = "1"
-    //     while (num < rows){
-    //     System.out.println(outpu1);
-    //     output1 = output1+"2";
-    
-    // }
+ 
+    public static void triangle(int rows){
+        int triangle = 0;
+        int output1 = 1;
+        while (triangle > rows){
+        System.out.println(output1);
+        output1 = output1+2;
+        }
+    }
     // public static void odds(int start){
     //     int num1 = 9;
-    //     for (int 1 = )
+    //     for (int 1 =  
     // }
     // public static void eo(int maxE){
 
