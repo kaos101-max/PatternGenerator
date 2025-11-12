e2af37bf0ab1df7cfb4830c3ca77833ba5ac613c
kaos101-max <watanabedaisuke616@gmail.com>
2025-11-12 19:29:11 +0000

    continue

--- Pattern.java
+++ Pattern.java
@@ -5,12 +5,14 @@ public class Pattern{
     //precondtion
     //postcondition
     public static void main(String[] args){
-        stars(7);
+        // stars(7);
         // triangle(9);
-        odds(9);
+        // odds(9);
+        eo(5);
+        // pyramid(5);
     }
     public static void stars(int rows){
-        //code goes here
+        // while loop
         int stars = 0;
         String output = "*";
         while (stars < rows){
@@ -20,18 +22,22 @@ public class Pattern{
         }
     }
  
-    // public static void triangle(int rows){
-    //     int trangle = 1;
-    //     while (rows < 0){
-    //     int output = num;
-    //     do{
-    //         System.out.println(num);
-    //         output--;
-    //     } while (output > 0);
-    //     System.out.println("");
-    //         num += 1;
-    //     }
-    // }
+    public static void triangle(int rows){
+        //while loop
+        int counts = rows;
+        int num = rows;
+        for (int i = rows; i > 0; i--) {
+            counts = num;
+            if (i % 2 == 1) {
+                do {
+                    System.out.print(i);
+                    counts--;
+                } while (counts > 0);
+                System.out.println("");
+            }
+            num--;
+        }
+    }
     public static void odds(int start){
         // for loop
         int ratio = start;
@@ -49,20 +55,48 @@ public class Pattern{
         }
         
     }
-    // public static void eo(int maxE){
-
-    // }
-    // public static void pyramid(int rows){
-
-    // }
-
-
-
-
-    // public EO (int max E){
-    // String letter = "E"
-    // if (max E % 2 == 0)
-    //{
-        //letter = "o"
-    //}}
+    public static void eo(int maxE){
+        //E
+        //O
+        //EE
+        //O
+        String letter = "E";
+        if(maxE%2==0){
+            letter = "O";
+        }
+        for(int i = 0; i < maxE; i++){
+            //another loop here
+            if (i%2 == 1){
+                
+            }
+            System.out.println(letter);
+            
+            if(letter.equals("E")){
+                letter = "O";
+            }
+            else{
+                letter = "E";
+            }
+        }
+    }
+    public static void pyramid(int rows){
+        // while loop
+        int num = 1;
+        int counts = rows * 2 - 1;
+        while (rows > 0) {
+            counts = rows * 2 - 1;
+            int spaces = num-1;
+            while(spaces > 0){
+                System.out.print(" ");
+                spaces--;
+            }
+            do {
+                System.out.print(num);
+                counts--;
+            }while(counts > 0);
+            System.out.println("");
+            num++;
+            rows--;
+        } 
+    }
 }
